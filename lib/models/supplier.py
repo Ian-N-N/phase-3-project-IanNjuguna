576aa6eb43859b5d15fb82f407fbc3ccfee11a00
Ian Njuguna <ian.njuguna@student.moringaschool.com>
2025-10-16 15:05:31 +0300

    first draft of supplier model

--- lib/models/supplier.py
+++ lib/models/supplier.py
@@ -7,3 +7,21 @@ class Supplier:
         self.name = name
         self.contact = contact
     
+    def save(self):
+        cursor.execute("INSERT INTO suppliers (name, contact) VALUES (?, ?)", (self.name, self.contact))
+        CONN.commit()
+        self.id = cursor.lastrowid
+# getting alll suppliers
+    @classmethod
+    def get_all(cls):
+        rows = cursor.execute("SELECT * FROM suppliers").fetchall()
+        return [cls(id=row[0], name=row[1], contact=row[2]) for row in rows]
+    
+    @classmethod
+    def find(cls, supplier_id):
+        row = cursor.execute("SELECT * FROM suppliers WHERE id = ?", (supplier_id,)).fetchone()
+        if row:
+            return cls(id=row[0], name=row[1], contact=row[2]) if row else None
+        
+    
+    
